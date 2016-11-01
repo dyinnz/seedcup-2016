ce070311b9eae2d8ec30737b8b534c046368f552
henrylee20 <443786959@qq.com>
2016-11-01 19:12:55 +0800

    add some commit

--- src/clike_interpreter.h
+++ src/clike_interpreter.h
@@ -8,15 +8,52 @@
 #include "ast.h"
 #include "variable_table.h"
 
+/**
+ * @brief   C-like programming langague Interpreter
+ *
+ * @details A Interpreter. It traverse a AST, and do as the AST descripted.
+ *
+ *          Its constructor needs a AST's root node.
+ *          When Exec() is executed, the interpreter will traverse th tree and record th line number it walked.
+ *          When Exec() returned, call OutptLines to output the result.
+ */
 class ClikeInterpreter {
  public:
+
+  /**
+   * @param A root node of the AST.
+   * @brief Interpreter Constructor. Need a AST's root and ready to exec
+   */
   ClikeInterpreter(Ast &&ast) : ast_(std::move(ast)) {}
 
+  /**
+   * @brief  Start interpret
+   */
   void Exec();
+
+  /**
+   * @param filename output file
+   * @brief output line number information
+   */
+  void OutputLines(const char *filename);
+
+ private:
+
+  /**
+   * record the line number information of this node
+   */
+  void recordLine(AstNode *node);
+
+  /**
+   * Exec statement(s)
+   */
   int ExecSingle(AstNode *node);
   void ExecBlock(AstNode *node);
   void ExecLoopBlock(AstNode *node);
 
+  /**
+   * Interprete specified syntactic structure
+   */
   void ExecTypeHead(AstNode *node);
   int ExecPrintf(AstNode *node);
 
@@ -28,12 +65,12 @@ class ClikeInterpreter {
   void ExecWhile(AstNode *node);
   void ExecDoWhile(AstNode *node);
 
+  /**
+   * Calculate a expression
+   */
   int EvalExpr(AstNode *node);
 
-  void OutputLines(const char *filename);
-
- private:
-
+private:
   Ast ast_;
   symbol_table::VariableTable table_;
   std::vector<size_t> run_lines_;
@@ -41,5 +78,4 @@ class ClikeInterpreter {
   bool is_break_;
   std::size_t last_line_;
 
-  void recordLine(AstNode *node);
 };
--- test/systest_in/loop_6.c
+++ test/systest_in/loop_6.c
@@ -2,7 +2,7 @@
 //int main() {
   int a, b = 1;printf("%d ", __LINE__);
 
-  for (a = printf("\\\"\"\'%%%d", 010); a; a = a - printf(" ")) {printf("%d ", __LINE__);
+  for (a = printf("\\\"\"\'%%%d", 010); printf("%d ", __LINE__),a; a = a - printf(" ")) {
     int a = -1;printf("%d ", __LINE__);
     while (b) {printf("%d ", __LINE__);
       printf("%d ", __LINE__);break;
